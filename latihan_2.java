4f4192df1650ebce2e22106b5d0c0520615894dc
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-11-13 11:55:13 +0700

    Create latihan_2.java

---
+++ latihan_2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zahra281022;
+
+/**
+ *
+ * @author HP
+ * 
+ * 
+ */
+public class latihan_2 {
+   public static void main(String[]args)
+    {
+        String[] hari = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        int i=0;
+        while(i<hari.length)
+            {
+                System.out.println(hari[i]);
+               i++;
+            }
+    }
+}
