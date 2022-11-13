964bd21ed6696541a233d6823ee1bf6d37910afc
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-11-13 11:55:46 +0700

    Create latihan_4.java

---
+++ latihan_4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zahra281022;
+
+/**
+ *
+ * Mengunakan for loop
+ * 
+ * @author HP
+ */
+public class latihan_4 {
+    public static void main(String[]args)
+    {
+        String[] hari = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        for(int i=0;i<hari.length;i++)
+            {
+                System.out.println(hari[i]);
+                ;
+            }
+    }
+}
